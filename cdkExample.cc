313165325277934a93074c33294d8dff72fefa6b
Ian Jarvis <ijarvis12@gmail.com>
2023-03-28 10:23:10 -0500

    io fix and comments

--- cdkExample.cc
+++ cdkExample.cc
@@ -1,27 +1,46 @@
 #include <cdk/cdk.h>
 
 int main(){
+        //initialize
         WINDOW *screen = initscr();
         CDKSCREEN *cdkscreen = initCDKScreen(screen);
         raw();
         echo();
-        int startx = 1;
-        int starty = 1;
-        CDKSWINDOW *swindow = newCDKSwindow(cdkscreen,startx,starty,25,50,"Hello, World",1000,true,true);
+
+        //starting place for swindow
+        int startrow = 1;
+        int startcol = 1;
+
+        //make swindow and display
+        CDKSWINDOW *swindow = newCDKSwindow(cdkscreen,startrow,startcol,25,50,"Hello, World",1000,true,true);
         drawCDKSwindow(swindow,true);
-        move(startx+=2,starty++);
+
+        //move cursor inside swindow
+        move(startrow,startcol++);
+
+        //refresh screen
         refreshCDKScreen(cdkscreen);
+
+        //command var
         char command[256] = {};
+
+        //loop for command exec
         do {
+                //clear command var
                 strcpy(command,"");
-                move(startx++,starty);
+                //move cursor down
+                move(startrow+=2,startcol);
+                //get command
                 getstr(command);
                 addCDKSwindow(swindow,command,BOTTOM);
                 refreshCDKScreen(cdkscreen);
-                //execCDKSwindow(swindow,command,**getCDKSwindowContents(swindow,NULL));
+                //exec command
+                execCDKSwindow(swindow,command,BOTTOM);
                 refreshCDKScreen(cdkscreen);
         }
         while(strcmp(command, "^C") != 0);
+
+        //end program
         destroyCDKSwindow(swindow);
         endCDK();
         return 0;
