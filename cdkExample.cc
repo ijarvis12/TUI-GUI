5312ef78e6df9c3d2f8fbb700c509c955b236b20
Ian Jarvis <ijarvis12@gmail.com>
2023-06-01 03:58:02 -0500

    Update cdkExample.cc
    addition of entry widget

--- cdkExample.cc
+++ cdkExample.cc
@@ -19,10 +19,16 @@ int main(void) {
         //cmdline PS1
         startcol++;
         startrow += 26;
-        char ps1[18] = "me@mycomputer $ ";
+        char ps1[17] = "me@mycomputer $ ";
+
+        //make entry widget and display it
+        CDKENTRY *entrywdgt = newCDKEntry(cdkscreen, startcol, startrow, "", ps1, 0, 0, vMIXED, 32, 0, 30, false, false);
+        drawCDKEntry(entrywdgt, false);
+        refreshCDKScreen(cdkscreen);
 
         //command var
-        char command[256] = {};
+        char command[32] = {};
+        char *cmd = command;
 
         //program end command
         char end_cmd[5] = "exit";
@@ -32,19 +38,19 @@ int main(void) {
 
         //loop for command exec
         while(strcmp(command, end_cmd)) {
-                //show cmdline
-                refreshCDKScreen(cdkscreen);
-                mvaddstr(startrow, startcol, ps1);
-                
-                //enter command
-                strcpy(command, "");
-                getstr(command);
-                
+                //get command
+                for(int i=0; i<32; i++) command[i] = 0;
+                cmd = activateCDKEntry(entrywdgt, NULL);
+                for(int i=0; i<strlen(cmd); i++) command[i] = cmd[i];
+
                 //exec command, with error catching
                 if (execCDKSwindow(swindow, command, BOTTOM)) {
                         addCDKSwindow(swindow, strcat(err, command), BOTTOM);
                         strcpy(err, "err: ");
                 }
+
+                //clear entry widget
+                cleanCDKEntry(entrywdgt);
         }
 
         //end program
