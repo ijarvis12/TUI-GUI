6c8e8d3ddc66fb52c63638d94d9ff08910db733f
Ian Jarvis <ijarvis12@gmail.com>
2023-11-04 12:44:53 -0500

    Update textualwm.py
    huge update to version 0.41 api and addition of textual-terminal dependency

--- textualwm.py
+++ textualwm.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
 
 from textual.app import App, ComposeResult
-from textual.widgets import TextLog
+from textual_terminal import Terminal
 from textual import events
 
-from rich.markup import MarkupError
+class Term(Terminal):
 
-import os, socket
-import pwd as _pwd
+    def __init__(self, command):
+        super().__init__(command)
 
-class Window(TextLog):
-
-    PS1 = '[blue]' + _pwd.getpwuid(os.geteuid())[0] + '@' + socket.gethostname() + '[/blue][white] >>>[/white] '
-
-    def __init__(self):
-        super().__init__(markup=True)
-        self.border_title = "Terminal"
-        self.x = int(str(self.styles.offset[0]))
-        self.y = int(str(self.styles.offset[1]))
-        self.border_subtitle = self.cmd = ""
-        self.cursor_idx = len(self.PS1)
+    def on_mount(self):
+        self.focus()
+        self.start()
 
     def on_mouse_down(self, event: events.MouseDown):
         if self != app.windows['above']:
@@ -50,70 +42,11 @@ class Window(TextLog):
                               (cmp_y * new_y + cmp_y * old_y + (not cmp_y) * old_y - (not cmp_y) * new_y) // 2,)
         self.focus()
 
-    def on_mount(self):
-        self.cmd = ""
-        self.border_subtitle = self.PS1
-        self.cursor_idx = len(self.PS1)
-        self.focus()
-
-    def update_subtitle(self, back: bool = False, add_char: bool = False, event_char: str = ""):
-        subtitle = self.border_subtitle
-        subtitle = subtitle.replace('[/blink]', '')
-        subtitle = subtitle.replace('[blink]', '')
-        if add_char:
-            self.border_subtitle = subtitle[:self.cursor_idx-1] + '[blink]' + event_char + '[/blink]' + subtitle[self.cursor_idx-1:]
-        else:
-            try:
-                self.border_subtitle = subtitle[:self.cursor_idx-1] + '[blink]' + subtitle[self.cursor_idx-1] + '[/blink]' + subtitle[self.cursor_idx+back*1:]
-            except MarkupError:
-                subtitle = self.border_subtitle
-                subtitle = subtitle.replace('[/blink]', '')
-                subtitle = subtitle.replace('[blink]', '')
-                subtitle = subtitle.strip(']')
-                subtitle = subtitle.strip('abcdefghijklmnopqrstuvwxyz')
-                subtitle = subtitle[:-2]
-                self.border_subtitle = subtitle[:self.cursor_idx-1] + '[blink]' + subtitle[self.cursor_idx-1] + '[/blink]' + subtitle[self.cursor_idx+back*1:]
-
-
     def on_key(self, event: events.Key):
-        event_char = event.character
-        event_key = event.key
-        if event_key == 'backspace' or event_key == 'delete':
-            if self.cursor_idx > len(self.PS1):
-                self.cursor_idx -= 1
-                self.update_subtitle(back=True)
-                self.cmd = self.cmd[:self.cursor_idx-len(self.PS1)] + self.cmd[self.cursor_idx-len(self.PS1)+1:]
-        elif event_key == 'left':
-            if self.cursor_idx > len(self.PS1) + 1:
-                self.cursor_idx -= 1
-                self.update_subtitle()
-        elif event_key == 'right':
-            if self.cursor_idx < len(self.PS1) + len(self.cmd):
-                self.cursor_idx += 1
-                self.update_subtitle()
-        elif event_key == 'ctrl+d':
+        if event.key == 'home':
+            app.add_window()
+        elif event.key == 'end':
             app.remove_window()
-        elif event_key == 'ctrl+l':
-            self.clear()
-            self.cmd = ""
-            self.border_subtitle = self.PS1
-            self.cursor_idx = len(self.PS1)
-        elif event_char == '\r':
-            try:
-                output = eval(self.cmd)
-                self.write(output)
-            except:
-                self.write(self.cmd)
-            self.cmd = ""
-            self.border_subtitle = self.PS1
-            self.cursor_idx = len(self.PS1)
-        elif event.is_printable == False:
-            pass
-        else:
-            self.cursor_idx += 1
-            self.update_subtitle(add_char=True, event_char=event_char)
-            self.cmd = self.cmd[:self.cursor_idx-len(self.PS1)-1] + event_char + self.cmd[self.cursor_idx-len(self.PS1)-1:]
-
 
 
 class WindowManager(App):
@@ -123,15 +56,12 @@ class WindowManager(App):
             max-height: 100%;
             max-width: 100%;
             overflow: hidden;
-            layers: temp below below1 below2 below3 below4 below5 below6 below7 below8 above;
+            layers: above temp below below1 below2 below3 below4 below5 below6 below7 below8;
         }
-        Window {
-            height: 20;
-            width: 40;
+        Terminal {
+            max-height: 20;
+            max-width: 50;
             border: white round;
-            content-align: left top;
-            scrollbar-gutter: stable;
-            scrollbar-size-vertical: 1;
         }
     """
 
@@ -155,7 +85,7 @@ class WindowManager(App):
                 del self.windows['above']
                 del temp_win
             # new window
-            self.windows['above'] = Window()
+            self.windows['above'] = Term(command="bash")
             self.windows['above'].styles.layer = 'above'
             if len(self.windows) > 1:
                 self.windows['above'].styles.offset = (int(str(self.windows[b_len_str].styles.offset[0])) + 2,
@@ -177,9 +107,5 @@ class WindowManager(App):
 
 
 if __name__ == "__main__":
-    cd = os.chdir
-    ls = os.listdir
-    pwd = os.getcwd
-    whoami = lambda: _pwd.getpwuid(os.geteuid())[0]
     app = WindowManager()
     app.run()
