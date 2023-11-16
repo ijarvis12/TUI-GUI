4b9f52b9fdb848d08e16b8d0b0fb0e0001a392f8
Ian Jarvis <ijarvis12@gmail.com>
2023-11-15 20:46:51 -0600

    Update pixman.py
    attempting to bend curses to my will with ansi codes for sixels

--- ncurses/pixman.py
+++ ncurses/pixman.py
@@ -2,69 +2,66 @@
 
 import curses
 import curses.ascii
+from cusser import Cusser
 
 class DisplayServer():
 
-        def __init__(self):
-                self.screen = curses.initscr()
+        def __init__(self, stdscr):
+                self.screen = stdscr = Cusser(stdscr)
+                #curses.noraw()
                 curses.curs_set(0)
                 curses.start_color()
                 curses.use_default_colors()
                 self.screen.attron(curses.A_BLINK)
-                self.buffer = []
-                for i in range(0,20):
-                        self.buffer.append([])
-                        for j in range(0,20):
-                                self.buffer[i].append([0,0,0])
 
         def __del__(self):
-                self.buffer = []
                 curses.endwin()
 
         def end(self):
                 self.__del__()
 
-        def set_pixel(self, y, x, rgb):
-                r, g, b = rgb
+        def set_pixel(self, y, x, rgb, blink=False):
+                r, g, b = rgb  # r,g,b each range 0 to 1000
                 curses.init_color(7, r, g, b)
                 curses.init_pair(1, 7, 0)
-                self.buffer[y][x] = [r,g,b]
-                self.screen.addch(y, x, ' ', curses.color_pair(1)) # note how this unsets blink
+                ch = '.' if blink else ' '
+                self.screen.addch(y, x, ch, curses.color_pair(1))
 
         def set_pixel_blink(self, y, x):
-                r,g,b = self.buffer[y][x]
-                curses.init_color(7,r,g,b)
-                curses.init_pair(1,7,0)
-                self.screen.addch(y, x, '.', curses.color_pair(1))
+                ch = self.screen.inch(y, x)
+                attr = ch & 0xFF00
+                self.screen.addch(y, x, '.', attr)
 
         def unset_pixel_blink(self, y, x):
-                r,g,b = self.buffer[y][x]
-                curses.init_color(7,r,g,b)
-                curses.init_pair(1,7,0)
-                self.screen.addch(y, x, ' ', curses.color_pair(1))
-
+                ch = self.screen.inch(y, x)
+                attr = ch & 0xFF00
+                self.screen.addch(y, x, ' ', attr)
 
         def pause(self):
                 self.screen.getch()
 
 
 def main(stdscr):
-        ds = DisplayServer()
+
+        ds = DisplayServer(stdscr)
 
         ds.screen.addstr(str(curses.COLOR_PAIRS))
 
+        # trying to add a sixel
+        ds.screen.addstr("\033Pq#0;2;100;0;0#0~\033\\")
+
         for i in range(1,20):
-                ds.set_pixel(i, i, (1000, 1000, 1000))
-                ds.set_pixel_blink(i , i)
+                ds.set_pixel(i, i, (1000, 1000, 1000), True)
 
         ds.pause()
 
-        for i in range(0,20):
+        for i in range(1,20):
                 ds.unset_pixel_blink(i, i)
 
         ds.pause()
 
         ds.end()
+
 
 
 if __name__ == '__main__':
