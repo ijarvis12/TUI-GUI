ea65973262e179bd812f32bdf6f6c55815dd0681
Ian Jarvis <ijarvis12@gmail.com>
2023-11-18 16:02:08 -0600

    Update pixman.py
    adding pixel buffer object (clear screen function on display server object doesn't work, no hiding of pixel buffers)

--- ncurses/pixman.py
+++ ncurses/pixman.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import curses
+import curses.panel
+
 
 class DisplayServer():
   """Because of the limitations of ncurses, we can only do 8 bit color for python3.9"""
@@ -35,8 +37,11 @@ class DisplayServer():
     # clear the rest of the color pairs (for get_pixel not mixing up)
     for cp in range(self.max_color_num+1, self.max_color_pair):
       curses.init_pair(cp, 0, 0)
+    # init pixel buffers
+    self.pixel_buffers = []
 
   def __del__(self):
+    self.clear_screen(True)
     curses.endwin()
 
   def end(self):
@@ -45,23 +50,47 @@ class DisplayServer():
   def pause(self):
     self.screen.getch()
 
-  def clear_screen(self):
-    maxy, maxx = self.screen.getmaxyx()
-    for i in range(maxy-1):
-      self.screen.hline(i, 0, ' ', maxx-1)  # hlines display faster than individual pixels
+  def clear_screen(self, remove_all_buffers=False):
+    for pb in self.pixel_buffers:
+      pb.buffer.hide()
+    if remove_all_buffers:
+      self.pixel_buffers = []
+    #maxy, maxx = self.screen.getmaxyx()
+    #for i in range(maxy-1):
+    #  self.screen.hline(i, 0, ' ', maxx-1)  # hlines display faster than individual pixels
 
-  def set_pixel(self, y, x, rgb=(0,0,0), set_blink=False):
+  def new_pixel_buffer(self, nlines=1, ncols=1, begin_y=0, begin_x=0):
+    pixel_buffer = PixelBuffer(self.screen, nlines, ncols, begin_y, begin_x)
+    self.pixel_buffers.append(pixel_buffer)
+    return pixel_buffer
+
+
+
+class PixelBuffer():
+  """Buffer of pixels that can be show and hidden, plus moved around"""
+
+  def __init__(self, screen, nlines=1, ncols=1, begin_y=0, begin_x=0):
+    #window = curses.newwin(nlines, ncols, begin_y, begin_x)
+    self.buffer = curses.panel.new_panel(screen)
+    self.buffer.top()
+    self.buffer.show()
+
+  def set_pixel(self, y=0, x=0, rgb=(0,0,0), set_blink=False):
+    # y, x relative to upper left corner of buffer
+    # Test if coordinates are right
+    if not self.buffer.window().enclose(y, x):
+      return
     # rgb - arithmetic, each range from 0 to 255
     r, g, b = rgb
-    r //= 3
-    g //= 3
-    b //= 3
+    r //= 3  # if using 24 bit color comment out this line
+    g //= 3  # ditto
+    b //= 3  # ditto
     color_num = int(b)
     color_num += int(g+(curses.COLORS/3)) if g != 0 else 0
     color_num += int(r+(curses.COLORS*(2/3))) if r != 0 else 0
     cp = color_num
     # get pixel blinking attr
-    already_blinking = (self.screen.inch(y, x) & curses.A_ATTRIBUTES) == curses.A_BLINK
+    already_blinking = (self.buffer.window().inch(y, x) & curses.A_ATTRIBUTES) == curses.A_BLINK
     # maybe set pixel blinking attr
     if already_blinking and not set_blink:
       if_set_blink = curses.A_BLINK
@@ -70,9 +99,9 @@ class DisplayServer():
     else:
       if_set_blink = 0x0
     # set pixel
-    self.screen.addch(y, x, '@', curses.color_pair(cp) | if_set_blink)
+    self.buffer.window().addch(y, x, '@', curses.color_pair(cp) | if_set_blink)
 
-  def set_sixel(self, y, x, rgb=(0,0,0), repeat=1, ch='~'):
+  def set_sixel(self, y=0, x=0, rgb=(0,0,0), repeat=1, ch='~'):
     # get binary bitmask of sixel
     binary = bin(ord(ch) - 63)[2:]
     # get pixels to set
@@ -85,7 +114,7 @@ class DisplayServer():
         self.set_pixel(y+j, x+i, rgb)
 
   def get_pixel(self, y, x):
-    char_and_attr = self.screen.inch(y, x)
+    char_and_attr = self.buffer.window().inch(y, x)
     is_blinking = (char_and_attr & curses.A_ATTRIBUTES) == curses.A_BLINK
     color_pair = (char_and_attr & curses.A_COLOR) // 2  # Bug in Python 3.9 / ncurses 5
     fg_color, _ = curses.pair_content(color_pair)
@@ -110,30 +139,34 @@ if __name__ == '__main__':
 
     ds = DisplayServer(stdscr)
 
-    ds.screen.addstr(str(curses.COLOR_PAIRS)+' '+str(curses.COLORS))
+    #ds.screen.addstr(str(curses.COLOR_PAIRS)+' '+str(curses.COLORS))
+
+    maxy, maxx = ds.screen.getmaxyx()
+
+    pb0 = ds.new_pixel_buffer(maxy-1, maxx-1)
 
     # trying to add a sixel
-    ds.set_sixel(2, 10, rgb=(100,100,100), repeat=5, ch='~')
+    pb0.set_sixel(2, 10, rgb=(100,100,100), repeat=5, ch='~')
 
     ds.pause()
 
     for i in range(1,10):
-      ds.set_pixel(i, i, (200,0,0))
+      pb0.set_pixel(i, i, (200,0,0))
 
     ds.pause()
 
     for i in range(10,20):
-      ds.set_pixel(i, i, (16,16,16))
+      pb0.set_pixel(i, i, (16,16,16))
 
     ds.pause()
 
     for i in range(1,20):
-      ds.set_pixel(i, i, (255,0,0), True)
+      pb0.set_pixel(i, i, (255,0,0), True)
 
     ds.pause()
 
     for i in range(1,10):
-      ds.set_pixel(i, i, (255,0,0))
+      pb0.set_pixel(i, i, (255,0,0))
 
     ds.pause()
 
@@ -145,21 +178,21 @@ if __name__ == '__main__':
         g = random.randrange(256)
         b = random.randrange(256)
         blnk = random.choice([True,False,False,False,False,False,False,False,False,False])
-        ds.set_pixel(i, j, (r,g,b), blnk)
+        pb0.set_pixel(i, j, (r,g,b), blnk)
 
     ds.pause()
 
-    maxy, maxx = ds.screen.getmaxyx()
+    pbmaxy, pbmaxx = pb0.buffer.window().getmaxyx()
 
-    for i in range(0,maxy-1):
-      for j in range(0,maxx-1):
-        ds.set_pixel(i, j, (255,255,255))
+    for i in range(0,pbmaxy-1):
+      for j in range(0,pbmaxx-1):
+        pb0.set_pixel(i, j, (255,255,255))
 
     ds.pause()
 
-    is_blink, rgb = ds.get_pixel(5,5)
-    ds.screen.addstr(7,7, str(rgb))
-    ds.screen.addstr(8,8, str(is_blink))
+    is_blink, rgb = pb0.get_pixel(5,5)
+    pb0.buffer.window().addstr(7,7, str(rgb))
+    pb0.buffer.window().addstr(8,8, str(is_blink))
 
     ds.pause()
 
