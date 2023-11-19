b4b5ffd9c8928f8124242e8109c810008223599b
Ian Jarvis <ijarvis12@gmail.com>
2023-11-19 13:20:11 -0600

    Update pixman.py
    updated prog to python 3.11 standards for curses module
    
    using python 3.11 now!  24 bit color, yay!

--- ncurses/pixman.py
+++ ncurses/pixman.py
@@ -15,11 +15,11 @@ class DisplayServer():
     curses.noecho()
     # init color
     curses.start_color()
-    self.max_color_num = curses.COLORS - 1
-    self.max_color_pair = (curses.COLOR_PAIRS - 1) // 2  # Bug in python3.9 / ncurses 5, only goes to 32k (not 64k)
+    self.MAX_COLOR_NUM = curses.COLORS - 1
+    self.MAX_COLOR_PAIR = curses.COLOR_PAIRS - 1
     # init black and white colors
     curses.init_color(0, 0, 0, 0)  # black
-    curses.init_color(self.max_color_num, 1000, 1000, 1000)  # white
+    curses.init_color(self.MAX_COLOR_NUM, 1000, 1000, 1000)  # white
     # init r,g,b colors
     color_num = 0
     color_range_step = 3000 // int(curses.COLORS**(1/3))
@@ -30,12 +30,12 @@ class DisplayServer():
           color_num += 1
           curses.init_color(color_num, r, g, b)
     # init color pairs (use black background)
-    for color in range(1, self.max_color_num):
+    for color in range(1, self.MAX_COLOR_NUM):
       curses.init_pair(color, color, 0)
     # init pair white on black
-    curses.init_pair(self.max_color_num, self.max_color_num, 0)
-    # clear the rest of the color pairs (for get_pixel not mixing up)
-    for cp in range(self.max_color_num+1, self.max_color_pair):
+    curses.init_pair(self.MAX_COLOR_NUM, self.MAX_COLOR_NUM, 0)
+    # clear the rest of the color pairs (for PixelBuffer.get_pixel() not mixing up)
+    for cp in range(self.MAX_COLOR_NUM+1, self.MAX_COLOR_PAIR):
       curses.init_pair(cp, 0, 0)
     # init pixel buffers
     self.pixel_buffers = []
@@ -88,9 +88,6 @@ class PixelBuffer():
       return
     # rgb - arithmetic, each range from 0 to 255
     r, g, b = rgb
-    r //= 3  # if using 24 bit color comment out this line
-    g //= 3  # ditto
-    b //= 3  # ditto
     color_num = int(b)
     color_num += int(g+(curses.COLORS/3)) if g != 0 else 0
     color_num += int(r+(curses.COLORS*(2/3))) if r != 0 else 0
@@ -123,7 +120,7 @@ class PixelBuffer():
     # y, x are relative to upper left corner of buffer
     char_and_attr = self.buffer.window().inch(y, x)
     is_blinking = (char_and_attr & curses.A_ATTRIBUTES) == curses.A_BLINK
-    color_pair = (char_and_attr & curses.A_COLOR) // 2  # Bug in Python 3.9 / ncurses 5
+    color_pair = char_and_attr & curses.A_COLOR
     fg_color, _ = curses.pair_content(color_pair)
     rgb = curses.color_content(fg_color) # rgb is a 3-tuple ranging from 0 to 1000
     r, g, b = rgb
